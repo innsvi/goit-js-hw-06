59ae0346caf0032b81c6de826218b45169c0a6e8
Inna Svichkar <112700457+innsvi@users.noreply.github.com>
2023-02-13 15:33:25 +0100

    Fixed colors change

--- js/task-09.js
+++ js/task-09.js
@@ -17,7 +17,3 @@ function onColorChange(e) {
   colorIndicatorEl.textContent = newColor;
   document.body.style.backgroundColor = newColor;
 }
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
