505b7e8d9f59d52d407d61fb692ab9b8a61cd225
Inna Svichkar <112700457+innsvi@users.noreply.github.com>
2023-02-13 15:55:09 +0100

    Update 5 task

--- js/task-05.js
+++ js/task-05.js
@@ -5,11 +5,28 @@
 const inputName = document.querySelector('#name-input');
 const greetName = document.querySelector('#name-output');
 
-inputName.addEventListener('input',(event) =>{
-    const inputText = event.currentTarget.value
-    greetName.textContent = inputText;
+const onNameInput = event =>{
+    console.log(event.currentTarget.value)
 
-    if(!inputText.length){
-        greetName.textContent = 'Anonymous'
+    if(event.currentTarget.value.length !== 0){
+        greetName.textContent = event.currentTarget.value
+    } 
+    else{
+        greetName.textContent = "Anonymous"
     }
-})
+}
+
+inputName.addEventListener('input',onNameInput)
+
+
+
+
+
+// inputName.addEventListener('input',(event) =>{
+//     const inputText = event.currentTarget.value
+//     greetName.textContent = inputText;
+
+//     if(!inputText.length){
+//         greetName.textContent = 'Anonymous'
+//     }
+// })
